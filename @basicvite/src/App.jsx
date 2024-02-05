0c7653f5eb9d47f1b1cd4ee37b40fffa40b274d0
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-02-05 18:14:32 +0530

    added files

---
+++ @basicvite/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import './App.css'
+ const App = () =>{
+  <div className='App'>
+    <h3>Haash just got started with react and it seems a little scary to her</h3>
+  </div>
+ }
+
+
+export default App
