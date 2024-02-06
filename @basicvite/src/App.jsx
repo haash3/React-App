7d8f5c8b784d91de6698e877638e2d1399c6ade9
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-02-06 15:55:24 +0530

    Modified files

--- @basicvite/src/App.jsx
+++ @basicvite/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 
 import './App.css'
+
+
+
  const App = () =>{
-  <div className='App'>
-    <h3>Haash just got started with react and it seems a little scary to her</h3>
+  const name = 'Haash'
+  return(
+    <div className='App'>
+    <>
+    <h1>Harshitha</h1>
+    </>
+    
   </div>
- }
+ 
+  )
+  }
 
 
 export default App
